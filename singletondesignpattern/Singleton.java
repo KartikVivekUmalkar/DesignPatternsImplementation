de96a5fef521950366eaf61ddca871d3c10d241b
KartikVivekUmalkar <kartikumalkar1994@gmail.com>
2020-08-20 15:08:34 -0700

    Adding comments to the code

--- singletondesignpattern/Singleton.java
+++ singletondesignpattern/Singleton.java
@@ -2,12 +2,17 @@ package com.singletondesignpattern;
 
 public class Singleton {
 
-	private static Singleton instance; // Private static instance of the Singleton Class
+	// Private static instance of the Singleton Class
+	// For avoiding overhead of synchronized, we can perform eager loading, i,e creating a new instance while declaration itself.
+	private static Singleton instance; 
+	
 
 	private Singleton() {
 
 	}
 
+	// For multi-threading support make the getInstance method synchronized. 
+	
 	public static Singleton getInstance() {
 
 		if (instance == null) {
